fdc146336ba43f77185bf44d8784bcd67e0ffc0e
ronamarg <ronadalistan@gmail.com>
2025-04-14 22:02:59 +0800

    Update: Footer mainly

--- Client/src/components/Footer.jsx
+++ Client/src/components/Footer.jsx
@@ -1,122 +1,66 @@
-function Footer(){
-    return (
-        <>
-        <div className="container my-5">
-            <footer
-                className="text-center text-lg-start text-white"
-                style="background-color: #1c2331"
-            >
-        <section
-            className="d-flex justify-content-between p-4"
-            style="background-color: #6351ce"
-        >
-        <div className="me-5">
-            <span>Get connected with us on social networks:</span>
-        </div>
-        <div>
-            <a href="" className="text-white me-4">
-                <i className="fab fa-facebook-f"></i>
-            </a>
-            <a href="" className="text-white me-4">
-                <i className="fab fa-twitter"></i>
-            </a>
-            <a href="" className="text-white me-4">
-                <i className="fab fa-google"></i>
-            </a>
-            <a href="" className="text-white me-4">
-                <i className="fab fa-instagram"></i>
-            </a>
-            <a href="" className="text-white me-4">
-                <i className="fab fa-linkedin"></i>
-            </a>
-            <a href="" className="text-white me-4">
-                <i className="fab fa-github"></i>
-            </a>
-        </div>
+import React from 'react';
+import '../css/footer.css';
+import Logo from "../images/logo.png";
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <footer className="footer">
+        {/* Section: Links */}
+        <section>
+          <div className="container text-center text-md-start mt-5">
+            <div className="row mt-3">
+              <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                <img src={Logo} alt="Servease Logo" width="200" margin="10rem"/>
+                <p className="text-white" style={{paddingBottom:"30"}}>
+                  Our vision is to provide convenience <br></br>and help increase your sales business. 
+                </p>
+              </div>
+
+              <div className="col-md-8 col-lg-8 col-xl-8 mx-auto mb-4 ml-auto">
+                <div className="row">
+
+                  <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4" style={{paddingLeft:"100"}}>
+                    <h6 className="title">About</h6>
+                    <br />
+                    <p><a href="#!" className="text-white">How it works</a></p>
+                    <p><a href="#!" className="text-white">Featured</a></p>
+                    <p><a href="#!" className="text-white">Partnership</a></p>
+                    <p><a href="#!" className="text-white">Business Relation</a></p>
+                  </div>
+
+                  <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                    <h6 className="title">Community</h6>
+                    <br />
+                    <p><a href="#!" className="text-white">Events</a></p>
+                    <p><a href="#!" className="text-white">Blog</a></p>
+                    <p><a href="#!" className="text-white">Podcast</a></p>
+                    <p><a href="#!" className="text-white">Invite a friend</a></p>
+                  </div>
+
+                  <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                    <h6 className="title">Socials</h6>
+                    <br />
+                    <p><a href="#!" className="text-white">Discord</a></p>
+                    <p><a href="#!" className="text-white">Instagram</a></p>
+                    <p><a href="#!" className="text-white">Twitter</a></p>
+                    <p><a href="#!" className="text-white">Facebook</a></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </section>
-    
-        <section className="">
-            <div className="container text-center text-md-start mt-5">
-                <div className="row mt-3">
-                <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-                <h6 className="text-uppercase fw-bold">Company name</h6>
-                <hr
-                className="mb-4 mt-0 d-inline-block mx-auto"
-                style="width: 60px; background-color: #7c4dff; height: 2px"
-                />
-                <p> Here you can use rows and columns to organize your footer
-              content. Lorem ipsum dolor sit amet, consectetur adipisicing
-              elit.
-                </p>
-            </div>
-            <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
 
-                <h6 className="text-uppercase fw-bold">Products</h6>
-                <hr
-                    className="mb-4 mt-0 d-inline-block mx-auto"
-                    style="width: 60px; background-color: #7c4dff; height: 2px"
-                    />
-                <p>
-                <a href="#!" className="text-white">MDBootstrap</a>
-                </p>
-                <p>
-                <a href="#!" className="text-white">MDWordPress</a>
-                </p>
-                <p>
-                <a href="#!" className="text-white">BrandFlow</a>
-                </p>
-                <p>
-                <a href="#!" className="text-white">Bootstrap Angular</a>
-                </p>
-            </div>
-
-            <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-                <h6 className="text-uppercase fw-bold">Useful links</h6>
-                <hr
-                    className="mb-4 mt-0 d-inline-block mx-auto"
-                    style="width: 60px; background-color: #7c4dff; height: 2px"
-                />
-                <p>
-                    <a href="#!" className="text-white">Your Account</a>
-                </p>
-                <p>
-                    <a href="#!" className="text-white">Become an Affiliate</a>
-                </p>
-                <p>
-                    <a href="#!" className="text-white">Shipping Rates</a>
-                </p>
-                <p>
-                    <a href="#!" className="text-white">Help</a>
-                </p>
-            </div>
-            <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-                <h6 className="text-uppercase fw-bold">Contact</h6>
-                <hr
-                    className="mb-4 mt-0 d-inline-block mx-auto"
-                    style="width: 60px; background-color: #7c4dff; height: 2px"
-                />
-                <p><i className="fas fa-home mr-3"></i> New York, NY 10012, US</p>
-                <p><i className="fas fa-envelope mr-3"></i> info@example.com</p>
-                <p><i className="fas fa-phone mr-3"></i> + 01 234 567 88</p>
-                <p><i className="fas fa-print mr-3"></i> + 01 234 567 89</p>
-            </div>
-
-            </div>
+        {/* Copyright */}
+        <div className="footer-bottom">
+          <span>©2025. All rights reserved</span>
+          <span>Privacy & Policy     Terms & Conditions</span>
         </div>
-        </section>
-        <div
-            className="text-center p-3"
-            style="background-color: rgba(0, 0, 0, 0.2)"
-        >
-        © 2020 Copyright:
-            <a className="text-white" href="https://mdbootstrap.com/"
-            >MDBootstrap.com</a
-            >
-        </div>
-    </footer>
-        </div>
-            </>
-        );
-}
-export default Footer
 
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
--- Client/src/components/Home.jsx
+++ Client/src/components/Home.jsx
@@ -1,7 +1,28 @@
-import "../css/home.css"
-function Home(){
-    return(
-        <h1>Home</h1>
-    );
+import React from "react";
+import "../css/home.css"; 
+import "bootstrap/dist/css/bootstrap.min.css";
+import Tempbg from "../images/tempbg.jpg";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="content">
+        <h1>Welcome to ServEase</h1>
+        <p>Your trusted service provider</p>
+      </div>
+    </div>
+  );
 }
-export default Home
+
+function Body(){
+    
+}
+
+function Background() {
+return(
+  <div className="background">
+    <img src="Tempbg.jpg" alt="background"/>
+  </div>
+);
+}
+export default Home;
