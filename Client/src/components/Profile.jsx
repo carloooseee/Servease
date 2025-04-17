a495665db110feb56015ae539e82452a4e82e456
Carlos Laran <carloooseeen@gmail.com>
2025-04-17 23:34:35 +0800

    profile functionality

--- Client/src/components/Profile.jsx
+++ Client/src/components/Profile.jsx
@@ -1,8 +1,19 @@
-import "../css/home.css"
-function Profile(){
-    const user = "Carlos";
-    return(
-        <h1>Welcome, {user}!</h1>
-    );
+import "../css/home.css";
+
+function Profile() {
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  if (!user) {
+    return <h1>bruh u aint exist</h1>;
+  }
+
+  return (
+    <div className="profile-container">
+      <h1>Welcome, <strong>{user.first_name} {user.last_name}!</strong></h1>
+      <h1><strong>Email:</strong> {user.email}</h1>
+      <h1><strong>Phone Number:</strong> {user.phone_number}</h1>
+    </div>
+  );
 }
-export default Profile
+
+export default Profile;
